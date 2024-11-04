3ca0408667b24400c99088803ef186548dd65b3a
Szukács Marcell <spiglebach@gmail.com>
2024-11-04 20:06:06 +0100

    fix lombok removal

--- src/test/java/hu/spiglebach/commons/exception/util/CustomTestErrorCode.java
+++ src/test/java/hu/spiglebach/commons/exception/util/CustomTestErrorCode.java
@@ -2,18 +2,21 @@ package hu.spiglebach.commons.exception.util;
 
 import hu.spiglebach.commons.exception.ApiErrorCode;
 import hu.spiglebach.commons.exception.ApiException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.util.Map;
 
-@RequiredArgsConstructor
 public enum CustomTestErrorCode implements ApiErrorCode {
     TEST_ERROR_CODE(HttpStatus.BAD_REQUEST, "This is a test error")
     ;
 
     public final HttpStatus status;
     public final String message;
+
+    CustomTestErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 
     @Override
     public ApiException exception() {
